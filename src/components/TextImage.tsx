c5068c54330e004afca72b426a74db616bb7f005
Chris3riel <chrisurpined@gmail.com>
2024-09-04 11:10:30 -0600

    Integracion de TextImage.tsx

---
+++ src/components/TextImage.tsx
@@ -0,0 +1,14 @@
+function TextImage(){
+    return (<>
+        <section> 
+            <div className="position-relative min-">
+
+            </div>
+            <div>
+
+            </div>
+
+        </section>
+    </>);
+
+}
